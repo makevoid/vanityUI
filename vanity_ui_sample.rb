35aff71a5865ee4691f47f50fc8234e730e5f8d8
Francesco Canessa <makevoid@gmail.com>
2014-07-28 08:57:30 +0200

    more sample code

--- vanity_ui_sample.rb
+++ vanity_ui_sample.rb
@@ -1,9 +1,5 @@
-require 'open3'
-
 require 'fox16'
 include Fox
-
-PATH = File.expand_path "../", __FILE__
 
 NUMBERS = (1..9).to_a
 ALPHABET_LOWER = ("a".."z").to_a
@@ -16,68 +12,14 @@ SIZES = [27, 34]
 
 ALL_POSSIBLE_CHARS = ALPHABET_LOWER + ALPHABET_UPPER - EXCLUDED_CHARS
 
-class Vanitygen
-
-  # note: nickname refers to the vanity part of the public key
-
-  attr_reader :nickname, :public_key, :private_key
-
-  def initialize(nickname)
-    @nickname = nickname
-    out = generate_keypair
-    parse_keypair out
-  end
-
-  private
-
-  def vanitygen_cmd
-    # bin = "vanitygen"
-    bin = "vanitygen_osx"
-    "#{PATH}/bin/vendor/#{bin} 1#{@nickname}  2>&1"
-  end
-
-  def generate_keypair
-    cmd = vanitygen_cmd
-    # puts cmd
-    out = `#{cmd}`
-    # puts out
-    out
-  end
-
-  def generate_keypair
-    stdin, stdout, stderr, @wait_thr = Open3.popen3 vanitygen_cmd
-
-    @t = Thread.new {
-      while true
-        unless stderr.eof?
-          line = stderr.readline
-          puts line
-        end
-      end
-    }
-    ""
-  end
-
-  def parse_keypair(output)
-    @public_key   = output.match(/Address: (\w+)/)[0]
-    @private_key  = output.match(/Privkey: (\w+)/)[0]
-  end
-
-end
-
-
 class PasswordGenerator < FXMainWindow
   def initialize(app)
-    super(app, "Password generator", width: 400, height: 200)
+    super(app, "Password generator", width: 400, height: 120)
 
     frame = FXHorizontalFrame.new self
     FXLabel.new frame, "Number of characters in password:"
     size_field = FXTextField.new frame, 4
     size_field.text = SIZES.first.to_s
-
-    frame2 = FXHorizontalFrame.new self
-    nick_field = FXTextField.new frame2, 4
-
 
     # hFrame2 = FXHorizontalFrame.new(self)
     # specialChrsCheck = FXCheckButton.new(hFrame2, "Include special characters in password")
@@ -89,21 +31,18 @@ class PasswordGenerator < FXMainWindow
     generateButton = FXButton.new(hFrame3, "Generate")
     copyButton = FXButton.new(hFrame3, "Copy to clipboard")
 
-
     generateButton.connect(SEL_COMMAND) do
-      vg = vanitygen(nick_field.text)
-      textArea.removeText 0, textArea.length
-      textArea.appendText vg.public_key
+      textArea.removeText(0, textArea.length)
+      textArea.appendText(generatePassword(size_field.text.to_i, ALL_POSSIBLE_CHARS))
     end
   end
 
-  def generate_password_ui(pwLength, charArray)
-    pass = generate_password
-    # spacePassword pass
-  end
-
-  def vanitygen(nick)
-    @vanitygen = Vanitygen.new nick
+  def generatePassword(pwLength, charArray)
+    len = charArray.length
+    pass = (1..pwLength).map do
+      charArray[rand(len)]
+    end.join
+    spacePassword pass
   end
 
   def spacePassword(pass)
